e6a989aeea136264fca09811bc56e6482ca6cf2e
Atomik <138856192+atomikk0@users.noreply.github.com>
2024-01-30 15:51:39 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 "yes"
+
+
+author 'Atomik'
+description 'atomik-safezone'
+
+
+client_script 'client/client.lua'
+shared_script 'config.lua'
+
+
+files {
+    "ui/ui.html",
+    "ui/js/app.js",
+}
+
+ui_page "ui/ui.html"
